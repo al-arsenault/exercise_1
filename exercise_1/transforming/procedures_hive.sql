13c71101bebd7f311198c35b096990ced3f0f460
al-arsenault <a.arsenault81@gmail.com>
2016-02-28 21:24:29 -0500

    Update procedures_hive.sql

--- exercise_1/transforming/procedures_hive.sql
+++ exercise_1/transforming/procedures_hive.sql
@@ -2,7 +2,7 @@ CREATE TABLE procedure_rankings AS SELECT
  surveys_responses.*, hospitals.provider ID
 FROM
   surveys_responses
-  LEFT OUTER JOIN 
+  INNER JOIN 
   hospitals
 ON
  (surveys_responses.hospitalName = hospitals.hospitalName)
