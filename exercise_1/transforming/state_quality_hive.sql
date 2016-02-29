212da2cd49cdfe2bba6de42561ae87c1e788abb9
al-arsenault <a.arsenault81@gmail.com>
2016-02-28 21:24:59 -0500

    Update state_quality_hive.sql

--- exercise_1/transforming/state_quality_hive.sql
+++ exercise_1/transforming/state_quality_hive.sql
@@ -1,10 +1,10 @@
 CREATE TABLE state_quality AS SELECT
  hospitals.state, readmissions.score, effective_care.score
 FROM
-  hospitals LEFT OUTER JOIN readmissions  
+  hospitals INNER JOIN readmissions  
 ON
   (hospitals.providerID = readmissions.providerID) 
-LEFT OUTER JOIN
+INNER JOIN
    effective_care
 ON 
    (effective_care.providerID = hospitals.providerID)
